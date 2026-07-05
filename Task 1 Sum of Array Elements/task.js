2b0e183b910eacebe4fa3c568fcd5010e7cf31ef
Saad-khan001 <saadkhanofficial83@gmail.com>
2026-07-05 06:44:54 +0500

    Task 1 - Sum of Array Elements
    A basic JavaScript program that calculates the sum of all numbers in an array using variables and a for loop.

---
+++ Task 1 Sum of Array Elements/task.js
@@ -0,0 +1,36 @@
+// Task 1: Sum of Array Elements
+
+
+
+// Arrays
+
+// for Loop
+
+// Variables
+
+
+// Create a program that calculates the sum of all numbers in an array.
+
+
+
+// [10, 20, 30, 40, 50]
+
+
+
+// Sum = 150
+
+
+// Arrays
+let numbers = [10, 20, 30, 40, 50];
+
+// variable to store the sum
+let sum = 0;
+
+// for loop function
+
+for (let i = 0; i <= 4; i++) {
+    console.log(numbers[i]);
+    sum = sum + numbers[i];
+}
+
+console.log("Sum =", sum);
